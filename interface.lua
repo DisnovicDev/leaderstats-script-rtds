5bda770f402e1036d38387954761f9235c500294
kb-scripts <realnataforramentos@gmail.com>
2026-04-11 17:18:56 -0300

    Update interface.lua

--- interface.lua
+++ interface.lua
@@ -1,19 +1,20 @@
-local frame = script.Parent
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local remoteEvent = ReplicatedStorage:WaitForChild("StatUpdate")
+local remote = Instance.new("RemoteEvent")
+remote.Name = "ApplyFakeLag"
+remote.Parent = game.ReplicatedStorage
 
--- Função única para processar cliques em qualquer botão dentro deste Frame
-for _, child in pairs(frame:GetChildren()) do
-    if child:IsA("TextButton") then
-        child.MouseButton1Click:Connect(function()
-            
-            -- Aqui definimos a lógica baseada no nome ou texto do botão
-            if child.Name == "AddCashBtn" then
-                remoteEvent:FireServer("Cash", 10)
-            elseif child.Name == "MegaHackBtn" then
-                remoteEvent:FireServer("Cash", 1000)
-            end
-            
-        end)
-    end
-end
+remote.OnServerEvent:Connect(function(player, target, delayMs)
+	if not target.Character then return end
+	
+	local root = target.Character:FindFirstChild("HumanoidRootPart")
+	if not root then return end
+	
+	local duration = 2
+	local interval = delayMs / 1000
+	
+	local lastPos = root.Position
+	
+	for i = 1, duration / interval do
+		task.wait(interval)
+		root.CFrame = CFrame.new(lastPos)
+	end
+end)
